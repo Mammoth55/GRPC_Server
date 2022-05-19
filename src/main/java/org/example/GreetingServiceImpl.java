29182103206a84ae1a0267daf726c9e195f1e6a5
mamontov_viacheslav <slamam@mail.ru>
2022-05-20 00:27:20 +0600

    add stream support

--- src/main/java/org/example/GreetingServiceImpl.java
+++ src/main/java/org/example/GreetingServiceImpl.java
@@ -9,11 +9,17 @@ public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImpl
     @Override
     public void greeting(GreetingServiceOuterClass.HelloRequest request,
                          StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
-        System.out.println(request);
-        GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder()
-                .setGreeting("Hello from server, " + request.getName())
-                .build();
-        responseObserver.onNext(response);
+        for (int i = 0; i < 10_000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder()
+                    .setGreeting("Hello from server, " + request.getName())
+                    .build();
+            responseObserver.onNext(response);
+        }
         responseObserver.onCompleted();
     }
 }
